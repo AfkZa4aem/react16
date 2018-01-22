3fc16cddb90dfe5b1e7a5fe3fe5efdb4f4870a2a
Unknown <nastypinagrably@gmail.com>
2018-01-22 11:23:54 +0300

    using bind and arrow function in events

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -11,12 +11,12 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('was clicked');
     // DON'T DO THIS: this.state.person[0].name = 'Maximilian'
     this.setState({
       persons: [
-        { name: 'Maximilian', age: 32 },
+        { name: newName, age: 32 },
         { name: 'Pavel', age: 31 },
         { name: 'Stephanie', age: 27 }
       ]
@@ -32,14 +32,14 @@ class App extends Component {
       <div className="App">
         <h1>Hi! I'm React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('Maximilian!')}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}/>
         <Person 
           name={this.state.persons[1].name} 
           age={this.state.persons[1].age}
-          click={this.switchNameHandler} >My Hobbies: Racing</Person>
+          click={this.switchNameHandler.bind(this, 'Max!')} >My Hobbies: Racing</Person>
         <Person 
           name={this.state.persons[2].name} 
           age={this.state.persons[2].age}/>
