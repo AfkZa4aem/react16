0bb8eb0bcb342804e396cd201ceaf5ed9d9c39d6
Unknown <nastypinagrably@gmail.com>
2018-01-22 11:15:39 +0300

    passing method reference between components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,6 +3,26 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 32 },
+      { name: 'Pavel', age: 31 },
+      { name: 'Stephanie', age: 26 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('was clicked');
+    // DON'T DO THIS: this.state.person[0].name = 'Maximilian'
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 32 },
+        { name: 'Pavel', age: 31 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    });
+  }
+
   render() {
     // without JSX
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
@@ -11,9 +31,18 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi! I'm React App</h1>
-        <Person name="Max" age="32"/>
-        <Person name="Pavel" age="31">My Hobbies: Racing</Person>
-        <Person name="Stephanie" age="26"/>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler} >My Hobbies: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
     );
   }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -4,7 +4,7 @@ const person = (props) => {
   return (
     <div>
       {/* Using props */}
-      <p>I'm {props.name} and I am {props.age} years old!</p>
+      <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
       {/* get access to something between oppening and closing tags */}
       <p>{props.children}</p>
     </div>
