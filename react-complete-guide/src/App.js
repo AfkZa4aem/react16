b1b76d70d723be3af9ef006d38fdd74b483934c2
Unknown <nastypinagrably@gmail.com>
2018-01-22 19:32:42 +0300

    Handling dynamic content "The JavaScript Way"

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -50,32 +50,33 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     };
-    // without JSX
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
+    
+    let persons = null;
 
-    // with JSX
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age}/>
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Max!')}
+            changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age}/>
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi! I'm React App</h1>
         <p>This is really working!</p>
-        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {/* Usung ternary expression to show/hide div */}
-        {
-          this.state.showPersons ?
-            <div>
-              <Person 
-                name={this.state.persons[0].name} 
-                age={this.state.persons[0].age}/>
-              <Person 
-                name={this.state.persons[1].name} 
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, 'Max!')}
-                changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-              <Person 
-                name={this.state.persons[2].name} 
-                age={this.state.persons[2].age}/>
-            </div> : null
-        }
+        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>          
+        {persons}
       </div>
     );
   }
