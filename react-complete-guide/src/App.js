880ee639577e2ff21a94b750da1da2a96cb49117
Unknown <nastypinagrably@gmail.com>
2018-01-22 12:52:02 +0300

    using inline stylilng

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -34,6 +34,14 @@ class App extends Component {
   }
 
   render() {
+
+    const style = {
+      background: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     // without JSX
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
 
@@ -42,7 +50,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi! I'm React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Maximilian!')}>Switch Name</button>
+        <button style={style} onClick={() => this.switchNameHandler('Maximilian!')}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}/>
