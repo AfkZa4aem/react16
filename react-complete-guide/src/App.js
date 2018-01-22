64bced06eabc54f8806e29bfb97a435574dfb6c2
Unknown <nastypinagrably@gmail.com>
2018-01-22 19:04:23 +0300

    rendering content conditionally, using ternary operator

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       { name: 'Max', age: 32 },
       { name: 'Pavel', age: 31 },
       { name: 'Stephanie', age: 26 }
-    ]
+    ],
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -33,6 +34,13 @@ class App extends Component {
     });
   }
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    })
+  }
+
   render() {
 
     const style = {
@@ -50,18 +58,24 @@ class App extends Component {
       <div className="App">
         <h1>Hi! I'm React App</h1>
         <p>This is really working!</p>
-        <button style={style} onClick={() => this.switchNameHandler('Maximilian!')}>Switch Name</button>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}/>
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Max!')}
-          changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age}/>
+        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {/* Usung ternary expression to show/hide div */}
+        {
+          this.state.showPersons ?
+            <div>
+              <Person 
+                name={this.state.persons[0].name} 
+                age={this.state.persons[0].age}/>
+              <Person 
+                name={this.state.persons[1].name} 
+                age={this.state.persons[1].age}
+                click={this.switchNameHandler.bind(this, 'Max!')}
+                changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+              <Person 
+                name={this.state.persons[2].name} 
+                age={this.state.persons[2].age}/>
+            </div> : null
+        }
       </div>
     );
   }
